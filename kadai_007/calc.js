517871c9ad7be6975cc40c1a6e98505f01e06e53
somaso-ma <fsoma0928@outlook.jp>
2024-12-22 20:57:00 +0900

    kadai_007の完成

---
+++ kadai_007/calc.js
@@ -0,0 +1,13 @@
+// 変数の定義
+let num = 30;
+
+// 3の倍数かどうか
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+  console.log("3の倍数です");
+} else if (num % 5 === 0) {
+  console.log("5の倍数です");
+} else {
+  console.log(num);
+}
